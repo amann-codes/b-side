0f2a23b525322ce5993e0cb290cfa3521b8fb4f6
amann-codes <aman.prajapati3000@gmail.com>
2025-12-24 00:24:08 +0530

    feat: proxy for the redirecting user based on the session data

---
+++ proxy.ts
@@ -0,0 +1,26 @@
+import { auth } from "@/lib/auth";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export async function proxy(request: NextRequest) {
+    const session = await auth();
+    const { pathname } = request.nextUrl;
+    const isLoggedIn = !!session?.user;
+
+    const isAuthPage = pathname.startsWith("/auth");
+    if (isLoggedIn && isAuthPage) {
+        return NextResponse.redirect(new URL("/", request.url));
+    }
+
+    if (!isLoggedIn && !isAuthPage) {
+        return NextResponse.redirect(new URL("/auth", request.url));
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: [
+        "/((?!api|_next/static|_next/image|favicon.ico).*)",
+    ],
+};
