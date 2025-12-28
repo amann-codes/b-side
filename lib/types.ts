d524ec9a6b308eebbeb428a4b85f4cc8e9096450
amann-codes <aman.prajapati3000@gmail.com>
2025-12-28 23:26:17 +0530

    all the types for the app

--- lib/types.ts
+++ lib/types.ts
@@ -12,3 +12,101 @@ export type GoogleSignInBodyType = {
     image?: string | null;
   };
 };
+
+
+export interface Discussion {
+  id: string
+  entityId?: string
+  entityType?: EntityType
+  title: string
+  content: string
+  author: string
+  timestamp: Date
+  replies: number
+  likes: number
+}
+
+export interface Sample {
+  id: string
+  trackId: string
+  originalTrack: string
+  artist: string
+  timestamp: string
+}
+
+export interface CoverArtInspiration {
+  id: string
+  albumId: string
+  imageUrl: string
+  description: string
+  source?: string
+}
+
+export interface Performance {
+  id: string
+  entityId: string
+  entityType: EntityType
+  venue: string
+  date: string
+  description: string
+  videoUrl?: string
+}
+
+export type AlbumType = "single" | "album" | "compilation"
+
+export type EntityType = "artist" | "track" | "album"
+
+export type Entity =
+  | SpotifyArtistEntity
+  | SpotifyAlbumEntity
+  | SpotifyTrackEntity
+
+export type SpotifyImage = {
+  url: string
+  height: number
+  width: number
+}
+
+export type SpotifyTrack = {
+  id: string
+  name: string
+  type: "track"
+  popularity: number
+  album_type: AlbumType
+  album: SpotifyAlbum
+  artists: SpotifyArtist[]
+  duration_ms: number
+  explicit: boolean
+}
+
+export type SpotifyArtist = {
+  id: string
+  name: string
+  type: "artist"
+  genres: string[]
+  popularity: number
+  images: SpotifyImage[]
+}
+
+export type SpotifyAlbum = {
+  id: string
+  name: string
+  type: "album"
+  images: SpotifyImage[]
+  release_date: string
+  album_type: AlbumType
+  total_tracks: number
+
+}
+
+export type SpotifyArtistEntity = SpotifyArtist & {
+  entityType: "artist"
+}
+
+export type SpotifyAlbumEntity = SpotifyAlbum & {
+  entityType: "album"
+}
+
+export type SpotifyTrackEntity = SpotifyTrack & {
+  entityType: "track"
+}
